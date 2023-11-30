af990fce5460cc7296bf2aa4aeb6601dccf6ba4d
alkhonin <asa15@illinois.edu>
2023-11-30 14:21:52 -0600

    adding two chart and functions (uncompleted draft)

--- Draft.py
+++ Draft.py
@@ -4,8 +4,8 @@ import geopandas as gpd
 import matplotlib.pyplot as plt
 import geopandas as gpd
 import plotly.express as px
+import plotly.graph_objects as go
 from IPython.display import display
-
 
 
 def filter_and_aggregate(df: pd.DataFrame, year: int, ppp_version: int, column: str) -> pd.Series:
@@ -70,7 +70,6 @@ def create_choropleth_map(world: gpd.geodataframe.GeoDataFrame,
     fig.show()
 
 
-
 def time_series(df: pd.DataFrame, column: str, ppp_version: int) -> pd.DataFrame:
     """
     Preprocess the input DataFrame by selecting specific columns, grouping data by 'year' and 'country',
@@ -92,6 +91,7 @@ def time_series(df: pd.DataFrame, column: str, ppp_version: int) -> pd.DataFrame
 
     return grouped_data
 
+
 def plot_time_series(time_series_data: pd.DataFrame, selected_countries: list) -> None:
     """
     Create an interactive line plot for selected countries using Plotly Express.
@@ -110,6 +110,7 @@ def plot_time_series(time_series_data: pd.DataFrame, selected_countries: list) -
 
     fig.show()
 
+
 def create_pie_chart(labels, sizes, title='Pie Chart'):
     """
 
@@ -125,6 +126,112 @@ def create_pie_chart(labels, sizes, title='Pie Chart'):
     fig.show()
 
 
+def preprocess_poverty_and_GDP_data(poverty_file_path, gdp_file_path):
+    """
+    Load and preprocess poverty and GDP data.
+
+    :param poverty_file_path: Path to the CSV file containing poverty data.
+    :param gdp_file_path: Path to the Excel file containing GDP data.
+    :return: A merged DataFrame with preprocessed data.
+    """
+    # Load poverty data and calculate poverty rate
+    poverty_data = pd.read_csv(poverty_file_path)
+    poverty_data['population_under_poverty'] = (
+                                                       poverty_data[
+                                                           '‘cost of basic needs’ approach - number below poverty line'] /
+                                                       (poverty_data[
+                                                            '‘cost of basic needs’ approach - number above poverty line'] +
+                                                        poverty_data[
+                                                            '‘cost of basic needs’ approach - number below poverty line'])
+                                               ) * 100
+
+    # Rename entities to maintain consistency
+    poverty_data['Entity'] = poverty_data['Entity'].replace({
+        'Eastern Europe and former USSR': 'Eastern Europe',
+        'South and South-East Asia': 'South and Southeast Asia'
+    })
+
+    # Load GDP data, reshape it, and clean column names
+    gdp_data = pd.read_excel(gdp_file_path, header=1, nrows=21)
+    gdp_data = gdp_data.rename(columns={
+        'Unnamed: 0': 'Year',
+        'South and Southeast Asia)': 'South and Southeast Asia',
+        'Latin America and Carribean': 'Latin America and Caribbean',
+        'East Asia ': 'East Asia'
+    })
+    melted_gdp = gdp_data.melt(id_vars=['Year'], var_name='Entity', value_name='GDP_per_capita')
+
+    # Merge the poverty and GDP data
+    merged_data = pd.merge(melted_gdp, poverty_data, on=['Year', 'Entity'], how='inner')
+
+    # Clean and convert GDP data to numeric
+    merged_data['GDP_per_capita'] = (
+        merged_data['GDP_per_capita']
+        .str.replace('[^\d.]', '', regex=True)
+        .replace('', float('nan'))
+        .pipe(pd.to_numeric, errors='coerce')
+    )
+
+    return merged_data, poverty_data
+
+
+def create_area_chart(data, entity):
+    """
+    Create an area chart for a given entity.
+    """
+    entity_data = data[data['Entity'] == entity]
+    fig = px.area(entity_data, x='Year', y='population_under_poverty',
+                  labels={'population_under_poverty': 'Percentage of Population'},
+                  title=f'Average Share of Population Living in Extreme Poverty - {entity}')
+    fig.update_layout(
+        xaxis_title='Year', yaxis_title='Percentage of Population',
+        plot_bgcolor='white', yaxis=dict(gridcolor='lightgrey', range=[0, 100]),
+        xaxis=dict(gridcolor='lightgrey'))
+    fig.show()
+
+
+def create_individual_chart(data, region):
+    """
+    Create a line chart for a specified region.
+    """
+    filtered = data[(data['Entity'] == region) & (~data['GDP_per_capita'].isna())]
+    fig = px.line(filtered, x='GDP_per_capita', y='population_under_poverty', text='Year', markers=True,
+                  title=f'GDP per Capita vs. Poverty Rate Over Time - {region}')
+    fig.update_traces(textposition='top center')
+    fig.update_layout(xaxis_title='GDP per Capita', yaxis_title='Poverty Rate (%)', showlegend=False)
+    fig.show()
+
+
+def create_comparison_chart(data, regions, standout_region):
+    """
+    Create a comparison chart for multiple regions with one standout region.
+    """
+    fig = go.Figure()
+    for region in regions:
+        filtered = data[(data['Entity'] == region) & (~data['GDP_per_capita'].isna())]
+        line_width = 3 if region == standout_region else 1
+        opacity = 1 if region == standout_region else 0.5
+        fig.add_trace(go.Scatter(
+            x=filtered['GDP_per_capita'], y=filtered['population_under_poverty'],
+            mode='lines+markers', name=region,
+            line=dict(width=line_width, color='black' if region == standout_region else None),
+            opacity=opacity))
+    fig.update_layout(
+        title='GDP per Capita vs. Poverty Rate Over Time',
+        xaxis_title='GDP per Capita', xaxis_type='log', yaxis_title='Poverty Rate (%)',
+        hovermode='x unified', showlegend=True)
+    fig.add_annotation(
+        text=standout_region,
+        xref="paper", x=0.8,
+        yref="paper", y=0.8,
+        showarrow=False,
+        font=dict(size=20),
+        align="right",
+        xanchor="right", yanchor="top"
+    )
+    fig.update_xaxes(title_text='GDP per Capita', tickvals=[1000, 5000, 10000, 20000, 50000])
+    fig.show()
+
 
 if __name__ == '__main__':
     # Load the dataset
@@ -132,16 +239,19 @@ if __name__ == '__main__':
     world = gpd.read_file('dataset/World_Countries_Generalized.geojson')
     world = replace_country_names(world)
     result_2019_2011_info = filter_and_aggregate(df, 2019, 2011, 'headcount_ratio_3000')
-    create_choropleth_map(world,result_2019_2011_info,'headcount_ratio_3000','% of population living in households with an income or expenditure per person below $30 a day.',)
+    create_choropleth_map(world, result_2019_2011_info, 'headcount_ratio_3000',
+                          '% of population living in households with an income or expenditure per person below $30 a day.', )
 
     selected_countries = ['India', 'Poland', 'Spain', 'South Korea', 'Denmark', 'Norway']
     time_series_data = time_series(df, 'headcount_ratio_3000', 2011)
-    plot_time_series(time_series_data,selected_countries)
+    plot_time_series(time_series_data, selected_countries)
 
+
+    filtered_df = df[(df['year'] == 2019) & (df['country'] == "World") ]
     labels = ['Less than $30', 'More than $30']
-    filtered_df = df[(df['year'] == 2019) & (df['reporting_level'] == "national")]
     sizes = [filtered_df['headcount_ratio_3000'].median(), 100 - filtered_df['headcount_ratio_3000'].median()]
     create_pie_chart(labels, sizes, title='$30-a-day-poverty-line')
+
     labels = ['Below $2.15 a day', '2.15 - $10 a day', '10 - $30 a day', 'Above $30 a day']
     sizes = [
         filtered_df['headcount_ratio_international_povline'].median(),
@@ -151,3 +261,15 @@ if __name__ == '__main__':
     ]
     create_pie_chart(labels, sizes, title='$2.15 poverty line')
 
+
+    regions = [
+        "World", "Western Europe", "Western Offshoots",
+        "Middle East and North Africa", "Eastern Europe", "Sub-Saharan Africa",
+        "East Asia", "South and Southeast Asia", "Latin America and Caribbean"
+    ]
+    file2, file1 = preprocess_poverty_and_GDP_data(
+        'dataset/share-of-population-living-in-extreme-poverty-cost-of-basic-needs.csv',
+        'dataset/GDP_by_region.xlsx')
+    create_area_chart(file1, "World")
+    create_individual_chart(file2, "World")
+    create_comparison_chart(file2, regions, "World")
